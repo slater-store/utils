c45fcfc25bcf46072f779ba9a653d933caeb67b6
estrattonbailey <ericstrattonbailey@gmail.com>
2020-01-22 10:13:19 -0600

    add getFileKey case

--- tests/getFileKey.test.js
+++ tests/getFileKey.test.js
@@ -7,6 +7,11 @@ test('works', t => {
   t.is(key, `snippets/foo.liquid`);
 });
 
+test('asset', t => {
+  const key = getFileKey(`/src/assets/anyfilename.js`);
+  t.is(key, `assets/anyfilename.js`);
+});
+
 test('invalid filename', t => {
   const key = getFileKey(`/src/bar/foo.liquid`);
   t.is(key, null);
