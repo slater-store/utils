8814661fcda15a823ae089dacd43be8b32c4577e
estrattonbailey <ericstrattonbailey@gmail.com>
2020-01-26 18:48:58 -0600

    validate theme fields on mergeConfig

--- tests/mergeConfig.test.js
+++ tests/mergeConfig.test.js
@@ -8,11 +8,31 @@ const cwd = process.cwd();
 test('works', t => {
   const config = mergeConfig({
     themes: {
-      dev: {}
+      dev: {
+        id: '1',
+        password: '1',
+        store: '1',
+      }
     }
   });
 
   t.is(config.in, path.join(cwd, `src`));
   t.is(config.assets.in, path.join(cwd, `src/assets/compiled/index.js`));
   t.is(config.assets.out, path.join(cwd, `src/assets/`));
+  t.truthy(config.themes.dev.ignore);
+});
+
+test('validates theme fields', t => {
+  t.throws(() => {
+    mergeConfig({
+      themes: {
+        dev: {
+          password: '1',
+          store: '1',
+        }
+      }
+    });
+  }, {
+    message: 'dev theme error - the id field is required',
+  });
 });
